24c47e4e709d01d99851163b747c32351b84ee37
Darshan <darshan@alphansotech.com>
2016-11-22 17:59:27 +0530

    Added the Authorize.net AIM (Advanced Integration Method) payment processing code. To make the solution/repository smaller in size, I haven't included the Authorize.net PHP SDK. Please download the SDK via composer before using this code.
    P.S I have included the Composer.json file in the solution. Additionally, included the constants directory and file, which you might not get with composer setup.

---
+++ constants/Constants.php
@@ -0,0 +1,21 @@
+<?php
+namespace SampleCode;
+
+class Constants
+{
+	//merchant credentials
+	const MERCHANT_LOGIN_ID = "3yPyv86Z55c9";
+	const MERCHANT_TRANSACTION_KEY = "75S5Qj4wt88bdKjp";
+	
+	const RESPONSE_OK = "Ok";
+	
+	//Recurring Billing
+	const SUBSCRIPTION_ID_GET = "2930242";
+
+	//Transaction Reporting
+	const TRANS_ID = "2238968786";
+
+	const SAMPLE_AMOUNT = "2.23";
+}
+?>
+
